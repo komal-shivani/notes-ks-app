478bdd24e0f1097e8cbd041feb1d03f6526d5a1a
komalshivani <komalshivani2@gmail.com>
2019-06-24 22:41:06 -0400

    extra added features

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors=require('cors')
 const app=express()
 const port = process.env.PORT || 3001
 const path = require("path")
+var Negotiator = require('negotiator')
 const {mongoose}=require('./config/database')
 
 // 1st appoach
--- notes-client/src/index.js
+++ notes-client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter, Route,Link,Switch} from 'react-router-dom'
-import NotesList from'./components/notes/notesList'
+import NotesList from './components/notes/notesList'
 import NotesShow from './components/notes/notesShow'
 import NotesNew from './components/notes/notesNew'
 import NotesEdit from './components/notes/notesEdit'
